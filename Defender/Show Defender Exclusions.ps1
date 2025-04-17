cb76ca1419f0023a467a09880f2551a764899493
Ward Vissers <39764253+WardVissers@users.noreply.github.com>
2025-04-17 21:55:59 +0200

    Defender

--- Defender/Show Defender Exclusions.ps1
+++ Defender/Show Defender Exclusions.ps1
@@ -1,1 +1,5 @@
-# Hello
+# Show Path en Process from Defender
+$computer = $env:computername
+$exclusions = Get-MpPreference
+$exclusions.ExclusionPath | Out-File "C:\temp\$computer`_path.txt"
+$exclusions.ExclusionProcess | Out-File "C:\temp\$computer`_process.txt"
